fb5000df52b247d0886b4de0b9aea2113791c4ff
Naveen Ithapu <naveen.programming@gmail.com>
2019-04-23 14:46:05 +0530

    corrected size update callback type

--- index.d.ts
+++ index.d.ts
@@ -6,16 +6,21 @@ import { Component } from "react";
 import { WebViewProps } from "react-native-webview";
 import {ViewStyle} from "react-native";
 
-export interface Styles{
+export interface StylesFiles {
     href: string;
     type: string;
     rel: string;
 }
 
+export interface SizeUpdate {
+    width:number,
+    height:number
+}
+
 export interface AutoHeightWebViewProps extends WebViewProps {
-    onSizeUpdated: (size:number)=>void;
+    onSizeUpdated: (size: SizeUpdate)=>void;
     baseUrl: string;
-    files: Styles[];
+    files: StylesFiles[];
     style: ViewStyle;
     customScript: string;
     customStyle: string;
