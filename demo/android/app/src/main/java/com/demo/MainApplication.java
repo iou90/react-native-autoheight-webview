d661931f62b928ff0df7d4b4ea9d0808dbf50c95
iou90 <iou90@outlook.com>
2019-07-04 15:05:02 +0800

    upgrade rn to 0.60.0 for demo; upgrade react-native-webview to 6.3.1 for demo

--- demo/android/app/src/main/java/com/demo/MainApplication.java
+++ demo/android/app/src/main/java/com/demo/MainApplication.java
@@ -2,14 +2,13 @@ package com.demo;
 
 import android.app.Application;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -22,10 +21,11 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNCWebViewPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
     }
 
     @Override
