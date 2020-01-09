96a9dbc5bc972dc61dafef4279c6dd3a07dc3bc8
iou90 <iou90@outlook.com>
2020-01-09 16:26:59 +0800

    Remove unused codes

--- demo/config.js
+++ demo/config.js
@@ -1,7 +1,6 @@
 'use strict';
 
-import newsletter from "./newsletterZeit";
-// import newsletter from "./newsletterBahn";
+import newsletter from "./newsletterBahn";
 
 const autoHeightHtml0 = newsletter;//`<p style="font-weight: 400;font-style: normal;font-size: 21px;line-height: 1.58;letter-spacing: -.003em;"><a href="https://github.com/iou90/react-native-autoheight-webview">Tags</a> are great for describing the essence of your story in a single word or phrase, but stories are rarely about a single thing. <span style="background-color: transparent !important;background-image: linear-gradient(to bottom, rgba(146, 249, 190, 1), rgba(146, 249, 190, 1));">If I pen a story about moving across the country to start a new job in a car with my husband, two cats, a dog, and a tarantula, I wouldn't only tag the piece with "moving". I’d also use the <a href="http://x-squad.com">tags</a> "pets", "marriage", "career change", and "travel tips".</span></p>`;
 
