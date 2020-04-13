2e77900e426060fc1e57ef539672e27d3708668e
iou90 <iou90@outlook.com>
2020-04-13 15:03:21 +0800

    Upgrade rn to 0.62.2, rn-webview to 9.0.2

--- demo/.eslintrc.js
+++ demo/.eslintrc.js
@@ -2,11 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    'prettier/prettier': [
-      {
-        singleQuote: true
-      }
-    ],
     'comma-dangle': 'off'
   }
 };
