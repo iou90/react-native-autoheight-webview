e510c28482ead9da46a2a13d9ab815fbb9501493
Krisztiaan <krisz011@gmail.com>
2020-07-23 16:57:50 +0200

    Update index.d.ts
    Add WebView `class` methods to type

--- index.d.ts
+++ index.d.ts
@@ -30,4 +30,39 @@ export interface AutoHeightWebViewProps extends WebViewProps {
   scrollEnabledWithZoomedin: boolean;
 }
 
-export default class AutoHeightWebView extends Component<Partial<AutoHeightWebViewProps>> { }
+export default class AutoHeightWebView extends Component<Partial<AutoHeightWebViewProps>> {
+  /**
+   * Go back one page in the webview's history.
+   */
+  goBack(): void;
+
+  /**
+   * Go forward one page in the webview's history.
+   */
+  goForward(): void;
+
+  /**
+   * Reloads the current page.
+   */
+  reload(): void;
+
+  /**
+   * Stop loading the current page.
+   */
+  stopLoading(): void;
+
+  /**
+   * Extra Native Component Config.
+   */
+  extraNativeComponentConfig(): any;
+
+  /**
+   * Executes the JavaScript string.
+   */
+  injectJavaScript(script: string): void;
+
+  /**
+   * Focuses on WebView redered page.
+   */
+  requestFocus(): void;
+}
