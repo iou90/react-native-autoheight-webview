f986d609266539f549366dd9aa726fc636d26bb2
iou90 <iou90@outlook.com>
2020-09-01 15:32:53 +0800

    Fix declaration for AutoHeightWebView: https://github.com/iou90/react-native-autoheight-webview/issues/181

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,11 @@
 // Definitions by: Naveen Ithappu <https://github.com/naveen-ithappu>
 // TypeScript Version: ^3.9.7
 
-import WebView, { WebViewProps } from 'react-native-webview';
+import {Component} from 'react';
 
-import { StyleProp, ViewStyle } from 'react-native';
+import {WebViewProps} from 'react-native-webview';
+
+import {StyleProp, ViewStyle} from 'react-native';
 
 export interface StylesFile {
   href: string;
@@ -29,4 +31,64 @@ export interface AutoHeightWebViewProps extends WebViewProps {
   scrollEnabledWithZoomedin: boolean;
 }
 
-export default class AutoHeightWebView extends WebView<Partial<AutoHeightWebViewProps>> {}
+export default class AutoHeightWebView extends Component<
+  Partial<AutoHeightWebViewProps>
+> {
+  /**
+   * Go back one page in the webview's history.
+   */
+  goBack: () => void;
+
+  /**
+   * Go forward one page in the webview's history.
+   */
+  goForward: () => void;
+
+  /**
+   * Reloads the current page.
+   */
+  reload: () => void;
+
+  /**
+   * Stop loading the current page.
+   */
+  stopLoading(): void;
+
+  /**
+   * Extra Native Component Config.
+   */
+  extraNativeComponentConfig: () => any;
+
+  /**
+   * Executes the JavaScript string.
+   */
+  injectJavaScript: (script: string) => void;
+
+  /**
+   * Focuses on WebView redered page.
+   */
+  requestFocus: () => void;
+
+  /**
+   * Posts a message to WebView.
+   */
+  postMessage: (message: string) => void;
+
+  /**
+   * (Android only)
+   * Removes the autocomplete popup from the currently focused form field, if present.
+   */
+  clearFormData: () => void;
+
+  /**
+   * (Android only)
+   * Clears the resource cache. Note that the cache is per-application, so this will clear the cache for all WebViews used.
+   */
+  clearCache: (clear: boolean) => void;
+
+  /**
+   * (Android only)
+   * Tells this WebView to clear its internal back/forward list.
+   */
+  clearHistory: () => void;
+}
